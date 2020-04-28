20551721633e0e9a3f68ef55842c49f7e45be039
victor <victorwpetersen@gmail.com>
2020-04-28 10:13:23 +0200

    Victors commit

--- src/com/company/RequestHandler.java
+++ src/com/company/RequestHandler.java
@@ -5,7 +5,10 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.net.Socket;
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,6 +24,15 @@ public class RequestHandler implements Runnable {
             System.out.println("Error: " + exception.getMessage());
 
         }
+    }
+
+    public void String(File file) throws IOException {
+        FileInputStream fileInputStream = new FileInputStream(file);
+        byte[] bytes = new byte[(int) file.length()];
+        fileInputStream.read(bytes);
+        String encodedString = Base64.getEncoder().encodeToString(bytes);
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("1", encodedString);
     }
 
     @Override
