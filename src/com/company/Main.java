ecf2e5615ff2384c945506877aae09286a214613
HolmQ84 <badboy2287>
2020-04-28 09:26:58 +0200

    Martins commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,13 @@
 package com.company;
 
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
+
+    Map<Socket, Integer> requests = new HashMap<>();
+
 
     public static void main(String[] args) {
 
--- src/com/company/RequestHandler.java
+++ src/com/company/RequestHandler.java
@@ -1,5 +1,9 @@
 package com.company;
 
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+
 public class RequestHandler implements Runnable {
 
     @Override
