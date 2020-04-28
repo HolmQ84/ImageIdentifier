77529723e321f71c2841ffce38631472fdb8f953
HolmQ84 <badboy2287>
2020-04-28 10:53:14 +0200

    Martins commit

--- src/com/company/Client.java
+++ src/com/company/Client.java
@@ -1,13 +1,15 @@
 package com.company;
 
+import java.io.File;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 
 public class Client {
+    private File file;
+
     public void socket() {
         try {
-
             Socket socket = new Socket(InetAddress.getLocalHost().getHostAddress(), 6780);
             System.out.println("Connecting to server");
             PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
@@ -19,4 +21,11 @@ public class Client {
         }
     }
 
+    public void addFile(File file) {
+        this.file = file;
+    }
+
+    public File getFile() {
+        return this.file;
+    }
 }
