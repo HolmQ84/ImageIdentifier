457a277228592dfd176704a4c9c5bb09e8030140
Nmtur <n.m.tureczek@gmail.com>
2020-04-28 10:40:48 +0200

    Nicholas Commit Main og Client

--- src/com/company/Client.java
+++ src/com/company/Client.java
@@ -1,5 +1,21 @@
 package com.company;
 
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+
 public class Client {
+    try {
+
+        Socket socket = new socket(InetAddress.getLocalHost().getHostAddress(), 6780);
+        System.out.println("Connecting to server");
+        PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
+        printWriter.println("hello");
+
+
+
+    } catch (Exception exception) {
+        System.out.println("Error: " + exception.getMessage());
+    }
 
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,10 @@
 package com.company;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.HashMap;
@@ -17,6 +22,20 @@ public class Main {
             Socket socket = serverSocket.accept();
             requests.put(id, socket);
             id++;
+
+
+            // modtager String fra netværk
+            InputStreamReader isr = new InputStreamReader(socket.getInputStream());
+            BufferedReader bufferedReader = new BufferedReader(isr);
+
+            String modtagetString = bufferedReader.readLine();
+
+            // Laver string til JSON
+            JSONParser parser = new JSONParser();
+            Object object = parser.parse(modtagetString);
+            JSONObject jsonObject = (JSONObject)object;
+
+
 
         } catch (Exception exception) {
             System.out.println("Error: " + exception.getMessage());
