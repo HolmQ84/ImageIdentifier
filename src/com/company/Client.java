91dfeebeced9f24fe315ca2c0016728846c0961a
victor <victorwpetersen@gmail.com>
2020-04-28 10:43:40 +0200

    Victors commit

--- src/com/company/Client.java
+++ src/com/company/Client.java
@@ -5,17 +5,18 @@ import java.net.InetAddress;
 import java.net.Socket;
 
 public class Client {
-    try {
+    public void socket() {
+        try {
 
-        Socket socket = new socket(InetAddress.getLocalHost().getHostAddress(), 6780);
-        System.out.println("Connecting to server");
-        PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
-        printWriter.println("hello");
+            Socket socket = new Socket(InetAddress.getLocalHost().getHostAddress(), 6780);
+            System.out.println("Connecting to server");
+            PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
+            printWriter.println("hello");
 
 
-
-    } catch (Exception exception) {
-        System.out.println("Error: " + exception.getMessage());
+        } catch (Exception exception) {
+            System.out.println("Error: " + exception.getMessage());
+        }
     }
 
 }
