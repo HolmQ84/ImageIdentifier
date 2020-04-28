bc895b8aa499272f3d07b97a41c106f808de0f2f
HolmQ84 <badboy2287>
2020-04-28 09:46:40 +0200

    Martins commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,13 +7,17 @@ import java.util.Map;
 
 public class Main {
 
-    Map<Socket, Integer> requests = new HashMap<>();
+    private static Map<Integer, Socket> requests = new HashMap<>();
+    private static int id = 0;
 
     public static void main(String[] args) {
         try {
             ServerSocket serverSocket = new ServerSocket(6780);
             System.out.println();
             Socket socket = serverSocket.accept();
+            requests.put(id, socket);
+            id++;
+
         } catch (Exception exception) {
             System.out.println("Error: " + exception.getMessage());
         }
--- src/com/company/RequestHandler.java
+++ src/com/company/RequestHandler.java
@@ -1,12 +1,26 @@
 package com.company;
 
+import netscape.javascript.JSObject;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.File;
 import java.net.Socket;
 import java.util.HashMap;
 import java.util.Map;
 
 public class RequestHandler implements Runnable {
 
+    public RequestHandler(int id, File file) {
+        try {
+            JSONParser parser = new JSONParser();
+            Object object = parser.parse(String.valueOf(file));
+            JSONObject jsonObject = (JSONObject) object;
 
+        } catch (Exception exception) {
+            System.out.println("Error: " + exception.getMessage());
+        }
+    }
 
     @Override
     public void run() {
